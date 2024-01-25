383233b572f7016f2a708c3b2a901dc8c8567480
Amina <231599@astanait.edu.kz>
2024-01-25 22:14:53 +0600

    Second init

--- src/Student.java
+++ src/Student.java
@@ -1,21 +1,18 @@
-class Student extends Person {
+public class Student extends Person {
     private double gpa;
-    public Student(String name, String surname, double gpa) {
-        super(name, surname);
-        this.gpa = gpa;
-    }
-    public double getGpa() {
-        return gpa;
-    }
-    public void setGpa(double gpa) {
-        this.gpa = gpa;
+    public Student(String name , String surname , double gpa){
+        super(name,surname);
+        setGpa(gpa);
     }
     @Override
-    public String toString() {
-        return "Student: " + super.getId() + ". " + super.getName() + " " + super.getSurname();
+    public String toString(){
+        return getPosition()+ ": " + super.toString();
     }
     @Override
-    public double getPaymentAmount() {
-        return (gpa > 2.67) ? 36660.00 : 0.00;
+    public double getPaymentAmount(){
+        if(getGpa()>2.67) return 36660.00;
+        return 0;
     }
+    double getGpa(){return gpa;}
+    void setGpa(double gpa){this.gpa = gpa;}
 }
